1cc604f8f97e0e706b8c823fc12cfa286b329802
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-28 01:57:42 +0800

    refactor (Auth): restructure routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { json } from "body-parser";
-
+import {
+  currentUserRouter,
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+} from "./routes";
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Current user");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("listening auth services on port 3000");
