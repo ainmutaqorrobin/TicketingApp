741162590e92c1291d32e127b9b24c65582f710b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-28 01:30:00 +0800

    feat (ingress): add ingress config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Current user");
+});
+
 app.listen(3000, () => {
   console.log("listening auth services on port 3000");
 });
