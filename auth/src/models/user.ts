018be423d6e60d983e4921a6a6b31166444ddac6
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-30 00:42:56 +0800

    feat (Auth): add User model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserDoc extends Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends Model<UserDoc> {
+  build: (attrs: UserAttrs) => UserDoc;
+}
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
