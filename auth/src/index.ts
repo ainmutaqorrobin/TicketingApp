346878483e4dd20ea8e5d2b64e0af3d363509f88
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-01 23:30:18 +0800

    feat (Auth): add JWT and Cookie Session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -10,10 +10,18 @@ import {
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const DATABASE_NAME = "auth";
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -26,6 +34,8 @@ app.all("*", async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined");
+
   try {
     await mongoose.connect(
       `mongodb://auth-mongo-service:27017/${DATABASE_NAME}`
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,7 +3,7 @@ import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post(
@@ -29,6 +29,20 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    //generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //store in session
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
