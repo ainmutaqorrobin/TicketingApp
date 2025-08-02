d07d94c14e42a3c9e6ed5781a4706610ba291467
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-03 07:51:18 +0800

    refactor (Auth): use request validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("success");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Give password of user"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send("logged in");
+  }
+);
 
 export default router;
-
