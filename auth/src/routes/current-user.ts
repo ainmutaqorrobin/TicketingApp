94c68efae6a5435cbc808fc6ebfa004899794350
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-03 20:31:38 +0800

    feat (Auth > currentuser): add currentuser method

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,18 @@
 import express from "express";
+import { verify } from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
+  if (!req.session?.jwt) return res.send({ currentUser: null });
+
+  try {
+    const payload = verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
   res.send("success");
 });
 
