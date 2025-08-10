454f662b43fc3606895ce9858d3cfdc3f279c85b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-10 23:35:13 +0800

    feat (client): trying data fetch from server side

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,8 +1,20 @@
+import { API } from "../const/api";
 import Banana from "./banana";
+import axios from "axios";
 
-function index() {
-  console.log("test");
+function index({ isLoggedIn }) {
+  console.log(isLoggedIn);
   return <Banana />;
 }
+
+export const getServerSideProps = async () => {
+  try {
+    const { data } = await axios.get(API.CURRENT_USER);
+    return { props: { isLoggedIn: !!data.currentUser } };
+  } catch (error) {
+    console.log(error);
+    return { props: { isLoggedIn: false } };
+  }
+};
 
 export default index;
