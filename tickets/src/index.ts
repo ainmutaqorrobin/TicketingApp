590150e246714bc0736c6dde51b3cd117b932fa8
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-18 22:42:29 +0800

    infra (tickets): configure MONGO_URI in k8s

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,21 +1,19 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 
-const MONGO_SERVICE = "auth-mongo-service:27017";
-const DATABASE_NAME = "auth";
-
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
   try {
-    await mongoose.connect(`mongodb://${MONGO_SERVICE}/${DATABASE_NAME}`);
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
   }
 
   app.listen(3000, () => {
-    console.log("listening auth services on port 3000");
+    console.log("listening tickets services on port 3000");
   });
 };
 
