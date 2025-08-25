916822fa36438aa4c000877d16db6fcd491d036f
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-26 01:12:24 +0800

    feat (nats-testing): add listener & publishing data

--- nats-testing/src/publisher.ts
+++ nats-testing/src/publisher.ts
@@ -1,9 +1,20 @@
 import nats from "node-nats-streaming";
 
+console.clear();
+const data = {
+  id: "123",
+  title: "concert",
+  price: 20,
+};
+
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
+
+  stan.publish("ticket:created", JSON.stringify(data), () => {
+    console.log("Event published");
+  });
 });
