a3feb49b386f56b5de91219d8ad9968bfc77e047
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-26 23:46:23 +0800

    feat (nats-testing): add queu group

--- nats-testing/src/listener.ts
+++ nats-testing/src/listener.ts
@@ -1,18 +1,35 @@
+import { randomBytes } from "crypto";
 import nats, { Message } from "node-nats-streaming";
 
 console.clear();
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS connection closed");
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "queue-group-name",
+    options
+  );
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
