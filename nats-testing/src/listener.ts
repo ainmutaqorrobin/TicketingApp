c2fdc1c494740a2af593d220df8e48e6b40118cd
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-29 22:35:09 +0800

    refactor (nats-testing): - create base listener class - create ticket listener class

--- nats-testing/src/listener.ts
+++ nats-testing/src/listener.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from "crypto";
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -14,28 +15,7 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    //get all messages in the channel (even the old ones)
-    .setDeliverAllAvailable()
-    //remember the last message we have seen and start from there if we restart
-    .setDurableName("orders-service");
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    //load balance the messages between multiple instances of the same service
-    "queue-group-name",
-    options
-  );
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
