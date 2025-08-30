06ddda52bf23485476147ff6dcf5b29d8435d63b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-08-30 23:57:19 +0800

    feat (tickets): add nats wrapper and ticket created publisher

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,12 +1,14 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined");
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+    await natsWrapper.connect("ticketing", "12e1", "http://nats-service:4222");
+    await connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
