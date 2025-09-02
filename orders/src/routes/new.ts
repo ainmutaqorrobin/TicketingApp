42269ea9b1a6390507dab1fcef1b4c1385146748
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-02 22:24:40 +0800

    wip (orders): update new route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
 import { API } from "./const";
+import { requireAuth, validateRequest } from "@robin_project/common";
+import { body } from "express-validator";
+import { Types } from "mongoose";
 
 const router = express.Router();
 
-router.post(`${API}`, (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  API,
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export default router;
