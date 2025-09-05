e0f919f972b6f6839e5fa68878d98bac3aa9ab84
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-05 11:40:36 +0800

    feat (orders): add logic for create order

--- orders/src/routes/const.ts
+++ orders/src/routes/const.ts
@@ -1,1 +1,2 @@
 export const API = "/api/orders";
+export const EXPIRATION_WINDOW_SECONDS = 15 * 60; //15 minutes
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response } from "express";
-import { API } from "./const";
-import { requireAuth, validateRequest } from "@robin_project/common";
+import { API, EXPIRATION_WINDOW_SECONDS } from "./const";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@robin_project/common";
 import { body } from "express-validator";
 import { Types } from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -17,8 +25,33 @@ router.post(
       .withMessage("TicketId must be provided"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send({});
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    //Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError();
+
+    //Make sure that this ticket is not already reserved
+    //is the ticket we just found *and* the orders status is *not* cancelled.
+    //If we find an order from that means the ticket is reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) throw new BadRequestError("Ticket is already reserved");
+
+    //Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS); //15 minutes
+
+    //Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket,
+    });
+    await order.save();
+
+    res.status(201).send(order);
   }
 );
 
