b20a29ba0919eefaad4198eeb182c762444e0e1a
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-07 00:47:03 +0800

    feat (orders): add listing route

--- orders/src/routes/listing.ts
+++ orders/src/routes/listing.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
 import { API } from "./const";
+import { requireAuth } from "@robin_project/common";
+import { Order } from "../models/order";
 const router = express.Router();
 
-router.get(API, (req: Request, res: Response) => {
-  res.send({});
+router.get(API, requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket");
+
+  res.send(orders);
 });
 
 export default router;
