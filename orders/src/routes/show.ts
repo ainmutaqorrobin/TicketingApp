644599928f98449ee38359faa1260a1a2a462c49
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-07 17:29:28 +0800

    feat (orders): add single order route

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
 import { API } from "./const";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@robin_project/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get(`${API}/:orderId`, (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  `${API}/:orderId`,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+    
+    res.send(order);
+  }
+);
 
 export default router;
