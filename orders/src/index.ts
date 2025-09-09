d47c315393338f65e68e252c96cf17be2de797b1
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-10 00:20:55 +0800

    feat (orders): listen for Ticket created & updated events

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import { connect } from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined");
@@ -21,12 +23,18 @@ const start = async () => {
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
+
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed");
       process.exit();
     });
+
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
