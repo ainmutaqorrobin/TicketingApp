82bc21822d9c1ff43bc01bb536c2cc1fb1adc5e6
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-10 00:15:12 +0800

    wip (orders): add event listener

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,19 @@
+import { Listener, Subjects, TicketCreatedEvent } from "@robin_project/common";
+import { Message } from "node-nats-streaming";
+import { QUEUE_GROUP_NAME } from "./const";
+import { Ticket } from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
+
+  readonly queueGroupName: string = QUEUE_GROUP_NAME;
+
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = Ticket.build({ id, title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, Subjects, TicketUpdatedEvent } from "@robin_project/common";
+import { QUEUE_GROUP_NAME } from "./const";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+
+  readonly queueGroupName: string = QUEUE_GROUP_NAME;
+
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const { id, price, title } = data;
+
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) throw new Error("Ticket not found");
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
