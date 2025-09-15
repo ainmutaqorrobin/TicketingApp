22692c1ca0b15ae6761843d62de545fe3d08b17e
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-09-15 19:38:16 +0800

    test (tickets): publish update ticket event

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -54,3 +54,17 @@ it("acknowledge the message", async () => {
 
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it("publish a ticket updated event", async () => {
+  const { data, listener, msg } = await listenerSetup();
+
+  await listener.onMessage(data, msg as Message);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const ticketUpdatedData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(data.id).toEqual(ticketUpdatedData.orderId);
+});
