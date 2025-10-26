10a29ab6a9fc9b74d897bd72356f5daa1ca3eae0
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-10-26 21:52:29 +0800

    refactor (client): change div to fragment

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -12,12 +12,12 @@ function MyApp({ Component, pageProps }) {
   const shouldShowHeader = !noHeaderRoutes.includes(router.pathname);
   return (
     <UserProvider initialUser={pageProps.user}>
-      <div>
+      <>
         {shouldShowHeader && <Header />}
         <div className="container mt-3">
           <Component {...pageProps} />
         </div>
-      </div>
+      </>
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         strategy="afterInteractive"
