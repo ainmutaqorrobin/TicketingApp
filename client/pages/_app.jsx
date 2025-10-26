d9175d61079f65689e349d4f3a4bb7e8c942e8ab
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-10-26 21:31:22 +0800

    fixed (client): document is not defined

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,11 +1,15 @@
-import("bootstrap/dist/css/bootstrap.min.css");
-import("bootstrap/dist/js/bootstrap.bundle.min.js");
+import "bootstrap/dist/css/bootstrap.min.css";
+import Script from "next/script";
 import { UserProvider } from "../context/user-context";
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider initialUser={pageProps.user}>
       <Component {...pageProps} />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        strategy="afterInteractive"
+      />
     </UserProvider>
   );
 }
