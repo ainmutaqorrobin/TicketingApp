5ffdecd0ac4582a15d8dbbb3e891201a15a71f83
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-10-26 23:07:57 +0800

    refactor (client): remove unused code

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,14 +1,5 @@
-import { getCurrentUser } from "../lib/get-current-user";
-
 function HomePage() {
   return <h1>Home Page</h1>;
 }
-
-export const getServerSideProps = async ({ req }) => {
-  const { user } = await getCurrentUser(req.headers);
-  return {
-    props: { user },
-  };
-};
 
 export default HomePage;
