100de7989ab3c90e1bad43a7e138b5b43631225d
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-10-26 21:51:41 +0800

    fixed (client): header show in auth pages

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -2,13 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Script from "next/script";
 import { UserProvider } from "../context/user-context";
 import Header from "../components/pages/header";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const noHeaderRoutes = ["/auth/signin", "/auth/signup"];
+
+  const shouldShowHeader = !noHeaderRoutes.includes(router.pathname);
   return (
     <UserProvider initialUser={pageProps.user}>
-      <Header />
-      <div className="container mt-3">
-        <Component {...pageProps} />
+      <div>
+        {shouldShowHeader && <Header />}
+        <div className="container mt-3">
+          <Component {...pageProps} />
+        </div>
       </div>
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
