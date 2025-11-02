5588d563e1409ddc2597d5da95bea789d03c0f17
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-11-03 01:25:11 +0800

    feat (client): add fallback value for build client

--- client/lib/build-client.jsx
+++ client/lib/build-client.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { INGRESS_URL } from "../const/api";
 
-export const buildClient = ({ req }) => {
+export const buildClient = ({ req } = {}) => {
   if (typeof window === "undefined") {
     // SSR or server-side
     return axios.create({
